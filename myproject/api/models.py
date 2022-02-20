8487f858d9172c51ad15727a90642b89c5f1f464
R41Ryan <ryanhuynh64@gmail.com>
2022-02-19 18:21:56 -0700

    Added Serializers

--- myproject/api/models.py
+++ myproject/api/models.py
@@ -34,6 +34,7 @@ class Posting(models.Model):
     startAddress = models.CharField(max_length=255)
     destination = models.CharField(max_length=255)
     startTime = models.TimeField()
+    endTime = models.TimeField()
     driverId = models.ForeignKey(Driver, on_delete=CASCADE, related_name="driverPostingId")
     maxPassenger = models.IntegerField()
     numOfPassenger = models.IntegerField()
@@ -56,7 +57,7 @@ class Report(models.Model):
     def __str__(self):
         return str(self.reportNum)
 
-class Posting_Passenger(models.Model):
+class PostingPassenger(models.Model):
     postingId = models.OneToOneField(Posting, null=False, on_delete=CASCADE, related_name="postingPassengerPosting")
     idPassenger = models.OneToOneField(Posting, null=False, on_delete=CASCADE, related_name="postingPassengerPassenger")
 
@@ -65,163 +66,3 @@ class Posting_Passenger(models.Model):
 
     def __str__(self):
         return str(self.postingId) + " " + str(self.idPassenger)
-
-"""
-class Staff(models.Model):
-    employeeID = models.IntegerField(primary_key=True)
-    firstName = models.CharField(max_length=255)
-    lastName = models.CharField(max_length=255)
-    employeeType = models.CharField(max_length=255)
-
-    class Meta:
-        app_label='api'
-
-    def __str__(self):
-        return str(self.employeeID)
-
-class Patient(models.Model):
-    healthcareNum = models.IntegerField(primary_key=True)
-    firstName = models.CharField(max_length=255)
-    lastName = models.CharField(max_length=255)
-    primaryNurseId = models.ForeignKey(Staff, null=True, on_delete=SET_NULL, related_name='primaryNurse')
-    secondaryNurseId = models.ForeignKey(Staff, null=True, on_delete=SET_NULL, related_name='secondaryNurse')
-
-    class Meta:
-        app_label='api'
-
-    def __str__(self):
-        return str(self.healthcareNum)
-
-class Pharmaceutical(models.Model):
-    name = models.CharField(max_length=255)
-    numberInStock = models.IntegerField()
-    IdPharmaceutical = models.IntegerField(max_length=255, primary_key=True)
-
-    class Meta:
-        app_label='api'
-
-    def __str__(self):
-        return str(self.IdPharmaceutical)
-
-class Drug(models.Model):
-    DIN = models.IntegerField()
-    IdPharmaceutical = models.OneToOneField(Pharmaceutical, on_delete=CASCADE, primary_key=True)
-    
-    class Meta:
-        app_label='api'
-    
-    def __str__(self):
-        return str(self.DIN)
-
-class VitaminHerb(models.Model):
-    VIN = models.IntegerField()
-    IdPharmaceutical = models.OneToOneField(Pharmaceutical, on_delete=CASCADE, primary_key=True)
-    
-    class Meta:
-        app_label='api'
-    
-    def __str__(self):
-        return str(self.VIN)
-
-class MedicineCaseContains(models.Model):
-    patientHealthcareNum = models.ForeignKey(Patient, on_delete=CASCADE, related_name='medCasePatient')
-    IdPharmaceutical = models.ForeignKey(Pharmaceutical, on_delete=CASCADE, related_name='medCasePharmaceutical')
-    numberOfPharmaceutical = models.IntegerField()
-    
-    class Meta:
-        app_label='api'
-        unique_together = ('patientHealthcareNum', 'IdPharmaceutical')
-
-class MedicalUnit(models.Model):
-    Number = models.IntegerField(primary_key=True)
-
-    class Meta:
-        app_label='api'
-
-    def __str__(self):
-        return str(self.Number)
-
-class MedicalBed(models.Model):
-    Number = models.IntegerField()
-    medUnitNumber = models.ForeignKey(MedicalUnit, on_delete=CASCADE, related_name='medicalUnit')
-    occupyingPatientNum = models.ForeignKey(Patient, null=True, on_delete=SET_NULL, related_name='bedPatient')
-
-    class Meta:
-        app_label='api'
-        unique_together = ('Number', 'medUnitNumber')
-
-class Supplier(models.Model):
-    name = models.CharField(max_length=255, primary_key=True)
-
-    class Meta:
-        app_label='api'
-
-    def __str__(self):
-        return self.name
-
-class Delivery(models.Model):
-    number = models.AutoField(primary_key=True)
-    dateTime = models.DateTimeField(auto_now=True)
-    type = models.IntegerField()
-    medCase = models.ForeignKey(Patient, null=True, on_delete=CASCADE, related_name='deliveryMedCase')
-    supplier = models.ForeignKey(Supplier, null=True, on_delete=CASCADE, related_name='deliverySupplier')
-
-    class Meta:
-        app_label='api'
-
-    def __str__(self):
-        return self.number
-
-class Supplies(models.Model):
-    supplier = models.ForeignKey(Supplier, on_delete=CASCADE, related_name='suppliesSupplier')
-    delivery = models.ForeignKey(Delivery, on_delete=CASCADE, related_name='suppliesDelivery')
-
-    class Meta:
-        app_label='api'
-        unique_together = ('supplier', 'delivery')
-
-class SupplierLocation(models.Model):
-    supplier = models.ForeignKey(Supplier, on_delete=CASCADE)
-    location = models.CharField(max_length=255)
-
-    class Meta:
-        app_label='api'
-        unique_together=('supplier', 'location')
-
-class DeliveryContains(models.Model):
-    delivery = models.ForeignKey(Delivery, on_delete=CASCADE, related_name='containsDelivery')
-    pharmaceutical = models.ForeignKey(Pharmaceutical, on_delete=CASCADE, related_name='containsPharmaceutical')
-    numberOfPharmaceutical = models.IntegerField()
-
-    class Meta:
-        app_label='api'
-        unique_together=('delivery', 'pharmaceutical')
-
-class CarriedOutBy(models.Model):
-    delivery = models.OneToOneField(Delivery, primary_key=True, on_delete=CASCADE, related_name='carriedOutByDelivery')
-    pharmacist = models.ForeignKey(Staff, on_delete=CASCADE, related_name='carriedOutByPharmacist')
-    
-    class Meta:
-        app_label='api'
-
-class Diagnosis(models.Model):
-    doctor = models.ForeignKey(Staff, on_delete=CASCADE, related_name='diagnosisDoctor')
-    patient = models.ForeignKey(Patient, on_delete=CASCADE, related_name='diagnosisPatient')
-    pharmaceutical = models.ForeignKey(Pharmaceutical, on_delete=CASCADE, related_name='DiagnosisPharmaceutical')
-    numberOfPharmaceutical = models.IntegerField()
-
-    class Meta:
-        app_label='api'
-        unique_together=('patient', 'pharmaceutical')
-
-class NurseOrder(models.Model):
-    nurse = models.ForeignKey(Staff, on_delete=CASCADE, related_name='NurseOrderNurse')
-    patient = models.ForeignKey(Patient, on_delete=CASCADE, related_name='NurseOrderPatient')
-    pharmaceutical = models.ForeignKey(Pharmaceutical, on_delete=CASCADE, related_name='NurseOrderPharmaceutical')
-    numberOfPharmaceutical = models.IntegerField()
-    dateTime = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        app_label='api'
-        unique_together=('patient', 'pharmaceutical')
-"""
