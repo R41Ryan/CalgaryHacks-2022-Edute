0b136061085b18426954290d749b32a827f4c658
R41Ryan <ryanhuynh64@gmail.com>
2022-02-19 17:25:53 -0700

    Added models

--- myproject/api/models.py
+++ myproject/api/models.py
@@ -18,7 +18,7 @@ class Account(models.Model):
         return str(self.idSchool)
 
 class Driver(models.Model):
-    idSchool = models.ForeignKey(Account, on_delete=CASCADE, related_name='driverAccountId')
+    idSchool = models.OneToOneField(Account, primary_key=True, on_delete=CASCADE, related_name='driverAccountId')
     licenseNum = models.IntegerField()
     insurancePolicyNum = models.IntegerField()
     driverHistoryFilePath = models.FileField(upload_to="driverHistory")
@@ -30,8 +30,41 @@ class Driver(models.Model):
         return str(self.idSchool)
 
 class Posting(models.Model):
-    postingId = models.IntegerField(primary_key=True)
-    
+    postingId = models.AutoField(primary_key=True)
+    startAddress = models.CharField(max_length=255)
+    destination = models.CharField(max_length=255)
+    startTime = models.TimeField()
+    driverId = models.ForeignKey(Driver, on_delete=CASCADE, related_name="driverPostingId")
+    maxPassenger = models.IntegerField()
+    numOfPassenger = models.IntegerField()
+    status = models.CharField(max_length=255)
+
+    class Meta:
+        app_label='api'
+
+    def __str__(self):
+        return str(self.postingId)
+
+class Report(models.Model):
+    reportNum = models.AutoField(primary_key=True)
+    reporterId = models.ForeignKey(Account, null=False, on_delete=CASCADE, related_name="reporterId")
+    reportedId = models.ForeignKey(Account, null=False, on_delete=CASCADE, related_name="reportedId")
+
+    class Meta:
+        app_label='api'
+
+    def __str__(self):
+        return str(self.reportNum)
+
+class Posting_Passenger(models.Model):
+    postingId = models.OneToOneField(Posting, null=False, on_delete=CASCADE, related_name="postingPassengerPosting")
+    idPassenger = models.OneToOneField(Posting, null=False, on_delete=CASCADE, related_name="postingPassengerPassenger")
+
+    class Meta:
+        app_label='api'
+
+    def __str__(self):
+        return str(self.postingId) + " " + str(self.idPassenger)
 
 """
 class Staff(models.Model):
